ce184e6d69da0b9f1915844dc04edafb40c6f560
Wissanu.khong <wissanu.khong@gmail.com>
2020-08-29 23:03:05 +0700

    explain the code

--- dark-mode/app.js
+++ dark-mode/app.js
@@ -1,5 +1,16 @@
 function toggleDarkLight() {
     var body = document.getElementById("body");
     var currentClass = body.className;
-    body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+    body.className = currentClass ===
+        "dark-mode" ?
+        "light-mode" :
+        "dark-mode";
 }
+
+/**
+ * Explain the code
+ * create function 
+ * create variable body to storage the value from documnt.getElementById("body");
+ * create variable currentClass to storage (varibable previous ) body.className [current class is dark-mode]
+ * creck value with ternary condition 
+ */
